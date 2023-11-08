9a02cc9a51dcfcda4501a3b474b6bbe2bb873a43
Hussain Ali <hs.hirani110@gmail.com>
2023-11-08 17:13:25 +0100

    Fixes bug for redis connection in docker and externalizes config values

--- server/src/main/java/com/hussainali/server/config/RedisConfig.java
+++ server/src/main/java/com/hussainali/server/config/RedisConfig.java
@@ -1,6 +1,6 @@
 package com.hussainali.server.config;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.cache.annotation.EnableCaching;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -11,11 +11,15 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String REDIS_HOST;
+    @Value("${spring.data.redis.port}")
+    private Integer REDIS_PORT;
 
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        return new JedisConnectionFactory(redisStandaloneConfiguration);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(REDIS_HOST, REDIS_PORT);
+        return new JedisConnectionFactory(config);
     }
 
     @Bean
